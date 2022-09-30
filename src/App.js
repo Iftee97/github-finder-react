edf9ef4d234648bc93b992e4e5c3748a3b1c2731
Iftee97 <tawsifmasrur@gmail.com>
2022-09-30 09:56:21 +0600

    added GithubContext and provider for global state management

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { GithubProvider } from './context/github/GithubContext'
 
 // pages and components
 import Home from './pages/Home'
@@ -9,20 +11,22 @@ import Footer from './components/layout/Footer'
 
 const App = () => {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/notfound' element={<NotFound />} />
-            <Route path='/*' element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
+          <Navbar />
+          <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/notfound' element={<NotFound />} />
+              <Route path='/*' element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import React from "react"
 import UserResults from "../components/users/UserResults"
 
 const Home = () => {
